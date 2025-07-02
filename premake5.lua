5ac3d8930763ef583cf7a000279eb32761832180
Thomas Payne <tommypayne093@gmail.com>
2025-07-02 09:53:01 -0400

    Updated submods with premake forks

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+project "DirectX-Headers"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "include/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs {
+        "include",
+        "thirdparty/DirectX-Headers/include",
+        "thirdparty/Vulkan-Headers/include",
+        "rtxmu/include"
+    }
+
+    links {
+        "RTXMU",
+        "directX-Headers"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "on"
+        optimize "off"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "Speed"
+        symbols "off"
+
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "Speed"
+        symbols "off"
+
+
+    include "rtxmu"
+    include "DirectX-Headers"
